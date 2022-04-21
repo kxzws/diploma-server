ac2e3fa9f95c1bf0b9be8c28b5123f510839c377
Alena Bulanova <kxzws54@gmail.com>
2022-04-22 02:09:12 +0300

    feat: add get request for search birds and able cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
+const mysql2 = require("mysql2");
 const app = express();
 const port = 3000;
-const mysql2 = require("mysql2");
 
 const connection = mysql2
   .createConnection({
@@ -13,23 +14,35 @@ const connection = mysql2
   })
   .promise();
 
+app.use(cors());
+
 app.get("/birds", (req, res) => {
   connection
-  .query(
-    "SELECT idSpecies, speciesName, internationalName, shortName, weight, wingspan, birdSpecies.description FROM birdSpecies JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus;"
-  )
-  .then((result) => {
-    res.json(result[0]);
-  })
-  .catch((err) => {
-    res.json(err);
-  });
+    .query(
+      "SELECT idSpecies as num, speciesName as title, internationalName as interTitle, shortName as protectStatus, longName as abbr, length, weight, wingspan, birdSpecies.description as description FROM birdSpecies JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus;"
+    )
+    .then((result) => {
+      res.json(result[0]);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/birds/:bird", (req, res) => {
+  connection
+    .query(
+      "SELECT idSpecies as num, speciesName as title, internationalName as interTitle, shortName as protectStatus, longName as abbr, length, weight, wingspan, birdSpecies.description as description FROM birdSpecies JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus WHERE LOWER(speciesName) REGEXP LOWER(?);",
+      req.params.bird
+    )
+    .then((result) => {
+      res.json(result[0]);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// http://localhost:3000/
-
-// SELECT idSpecies, speciesName, internationalName, shortName, weight, wingspan, birdSpecies.description FROM birdSpecies JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus;
