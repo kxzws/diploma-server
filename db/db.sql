3e744df682d3d3ee0f2604db1b1e22b0feba9815
Alena Bulanova <kxzws54@gmail.com>
2022-05-11 19:24:00 +0300

    feat: add mysql db files

---
+++ db/db.sql
@@ -0,0 +1,102 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+CREATE DATABASE kursach;
+USE kursach;
+
+CREATE TABLE users ( -- хранит пользователей
+	idUser int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    userName varchar(35) NOT NULL,
+    userPass varchar(32) NOT NULL,
+    isAdmin boolean NOT NULL,
+    email varchar(50) NOT NULL,
+    phoneNumber varchar(20) NOT NULL
+    -- constraint phone
+    -- constraint mail
+);
+
+CREATE TABLE birdGenus ( -- хранит роды птиц
+	idGenus int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    genusName varchar(100) NOT NULL,
+	internationalName varchar(100),
+    foundYear int,
+    description varchar(256)
+);
+
+CREATE TABLE protectionStatus (
+	idPrS int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    shortName varchar(20) NOT NULL,
+    longName varchar(100) NOT NULL,
+    description varchar(256)
+);
+
+CREATE TABLE birdSpecies ( -- хранит виды птиц
+	idSpecies int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	speciesName varchar(100) NOT NULL,
+    length double, -- метры
+    weight double, -- граммы
+    wingspan double, -- размах крыльев, метры
+    description varchar(256),
+    idGenus int NOT NULL,
+    idPrS int NOT NULL,
+    FOREIGN KEY (idGenus)
+		REFERENCES birdGenus (idGenus),
+	FOREIGN KEY (idPrS)
+		REFERENCES protectionStatus (idPrS)
+);
+
+CREATE TABLE preserves ( -- хранит заповедники
+	idPres int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    presName varchar(100) NOT NULL,
+    presOwner varchar(50) NOT NULL,
+    area int NOT NULL, -- тыс. гектаров
+    foundYear int,
+    address varchar(100) NOT NULL
+);
+
+CREATE TABLE preserves2birdSpecies (
+	idPreserves2birdSpecies int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	idSpecies int NOT NULL,
+    idPres int NOT NULL,
+	FOREIGN KEY (idPres)
+		REFERENCES preserves (idPres),
+	FOREIGN KEY (idSpecies)
+		REFERENCES birdSpecies (idSpecies)
+);
+
+CREATE TABLE donates (
+	idUser int NOT NULL,
+    idPreserves2birdSpecies int NOT NULL,
+    donateDate datetime NOT NULL,
+    donateAmount int NOT NULL,
+    FOREIGN KEY (idUser)
+		REFERENCES users (idUser),
+	FOREIGN KEY (idPreserves2birdSpecies)
+		REFERENCES preserves2birdSpecies (idPreserves2birdSpecies)
+);
+
+DROP DATABASE kursach;
+
+/*
+CREATE TABLE address (
+	idAddr int PRIMARY KEY NOT NULL,
+    country varchar(100) NOT NULL,
+    city varchar(60),
+    region varchar(60)
+);
+*/
+
+/*
+CREATE TABLE userBirdPreserveDonates ( -- хранит связь между пользователями, птицами и заповедникам, а также информацию о донате
+	idUser int NOT NULL,
+    idBird int NOT NULL,
+    idPres int NOT NULL,
+	FOREIGN KEY (idUser)
+		REFERENCES birds (idUser),
+	FOREIGN KEY (idBird)
+		REFERENCES users (idBird),
+	FOREIGN KEY (idPres) 
+		REFERENCES preserves (idPres),
+	donateDate datetime NOT NULL,
+    donateCount int NOT NULL
+);
+*/
---
+++ db/requests.sql
@@ -0,0 +1,69 @@
+-- все птицы
+SELECT idSpecies, speciesName, internationalName, shortName, weight, wingspan, birdSpecies.description FROM birdSpecies
+JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS
+JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus
+ORDER BY speciesName ASC;
+
+
+-- все заповедники
+SELECT idPres as num, presName, presOwner, area, foundYear, address FROM preserves;
+
+
+SET @input_parameter := 'орл';
+-- поиск по названию
+SELECT idSpecies as num, speciesName as title, internationalName as interTitle, shortName as protectStatus, longName as abbr, length, weight, wingspan, birdSpecies.description as description FROM birdSpecies
+JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS
+JOIN birdGenus ON birdGenus.idGenus = birdSpecies.idGenus
+WHERE LOWER(speciesName) REGEXP LOWER(@input_parameter);
+
+
+-- вставка с известными primary keys видов, заповедника, размера доната и никнейма
+INSERT INTO donates (idUser, idPreserves2birdSpecies, donateDate, donateAmount)
+(SELECT (SELECT idUser FROM users
+WHERE userName = @nick),
+db.idPres2Spec,
+NOW(), @amount FROM birdSpecies
+JOIN (SELECT idPreserves2birdSpecies as idPres2Spec, idSpecies FROM preserves2birdSpecies
+WHERE idPres = @pres AND idSpecies IN (1, 3, 5)) db ON db.idSpecies = birdSpecies.idSpecies);
+SELECT * FROM donates;
+
+SET @species := '1, 3, 5';
+SET @pres := 1;
+SET @amount := 666;
+SET @nick := 'kxzws';
+
+SELECT (SELECT idUser FROM users
+WHERE userName = @nick),
+db.idPres2Spec,
+NOW(), @amount FROM birdSpecies
+JOIN (SELECT idPreserves2birdSpecies as idPres2Spec, idSpecies FROM preserves2birdSpecies
+WHERE idPres = @pres AND idSpecies IN (1, 3, 5)) db ON db.idSpecies = birdSpecies.idSpecies;
+
+
+SET @pass := '12345679';
+-- проверка верности пароля к юзеру
+SELECT userName, isAdmin FROM users
+WHERE userName = @nick AND userPass = md5(@pass);
+
+
+-- вставка нового вида птиц к существующему роду
+INSERT INTO birdSpecies (speciesName, length, weight, wingspan, description, idGenus, idPrS) VALUES
+('Милый', 1, 125, 2, 'Такой', 4, 1);
+INSERT INTO preserves2birdSpecies (idSpecies, idPres)
+(SELECT idSpecies, 4 FROM birdSpecies
+WHERE speciesName REGEXP '^Милый$');
+
+-- удаление определённого вида птиц
+DELETE FROM preserves2birdSpecies
+WHERE idSpecies = 13;
+DELETE FROM birdSpecies 
+WHERE idSpecies = 13;
+
+SELECT * FROM birdSpecies;
+SELECT * FROM preserves2birdSpecies;
+
+-- топ-3 донатеров
+SELECT DISTINCT userName as user, donateAmount as amount FROM donates
+JOIN users ON donates.idUser = users.idUser
+ORDER BY donateAmount DESC
+LIMIT 3;
