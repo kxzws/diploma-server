e6fb1868d46fd463ad9885073065d25efc60a714
Alena Bulanova <kxzws54@gmail.com>
2022-12-12 22:18:24 +0300

    feat: add finance route

--- index.js
+++ index.js
@@ -74,6 +74,26 @@ app.get("/rating", (req, res) => {
     });
 });
 
+// финансы
+app.get("/finance", (req, res) => {
+  const sql = `SELECT presName, annualStateBudget, IF(SUM(donateAmount) > 0, SUM(donateAmount), 0) as donateAmount, staffQty * staffPersonCost as staffCost,
+  SUM(speciesRepresentativesQty * maintenanceCost) as speciesCost FROM preserves
+  LEFT JOIN preserves2birdSpecies ON preserves2birdSpecies.idPres = preserves.idPres
+  LEFT JOIN donates ON donates.idPreserves2birdSpecies = preserves2birdSpecies.idPreserves2birdSpecies
+  LEFT JOIN birdSpecies ON birdSpecies.idSpecies = preserves2birdSpecies.idSpecies
+  LEFT JOIN protectionStatus ON protectionStatus.idPrS = birdSpecies.idPrS
+  GROUP BY presName;`;
+
+  connection
+    .query(sql)
+    .then((result) => {
+      res.json(result[0]);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 // получение всех птиц с сортировкой
 app.get("/birds/:sort", (req, res) => {
   let sortType;
